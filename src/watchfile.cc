94e556717cf210220d55f0bb7b75dfa48001ef41
Richard Kettlewell <rjk@greenend.org.uk>
2011-01-26 22:34:35 +0000

    Add a test for Watcher

--- src/watchfile.cc
+++ src/watchfile.cc
@@ -1,13 +1,17 @@
 #include "Watcher.h"
+#include <cerrno>
 
 class MyWatcher: public Watcher {
 public:
   MyWatcher(const std::string &path): Watcher(path) {}
   
   void processLine(const std::string &line) {
-    printf(">>> %s", line.c_str());
+    printf("%s", line.c_str());
     if(line.find('\n') == std::string::npos)
       printf(" [no trailing newline]\n");
+    fflush(stdout);
+    if(ferror(stdout))
+      throw std::runtime_error(std::string("error writing to stdout ") + strerror(errno));
   }
 };
 
